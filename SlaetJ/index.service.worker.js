e9f73cd96421aee94bcee8de6413e8e37cc1e6c6
JordanNiphan <slaet.gustave@gmail.com>
2025-05-23 12:32:02 -0700

    Up

--- SlaetJ/index.service.worker.js
+++ SlaetJ/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748020988|32838370';
+const CACHE_VERSION = '1748028673|7717480718';
 /** @type {string} */
 const CACHE_PREFIX = 'SlaetJ-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
